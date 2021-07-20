583799e41cd3d209608b312a27b20d529eeb83b8
Gagan Malvi <malvigagan@gmail.com>
2021-07-20 19:11:56 +0200

    phoenix: Update build fingerprint to July, 2021.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>
    Change-Id: Ic49bfe4e50e5f1525628e8ada33cd49376b103c6

--- statix_phoenix.mk
+++ statix_phoenix.mk
@@ -21,4 +21,4 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
